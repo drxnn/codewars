344e320e25e3b1daf26f21a80d35eadaceca02f6
Drin Bratotini <drin.bratotini@gmail.com>
2023-08-03 11:45:34 -0400

    kata 7 kyu sort

--- 7kyu.js
+++ 7kyu.js
@@ -233,3 +233,24 @@ var capitals = function (word) {
 // END OF E.9
 ///////
 //////
+
+
+
+
+
+// Exercise 10
+////
+//
+// Finish the solution so that it sorts the passed in array of numbers. If the function passes in an empty array or null/nil value then it should return an empty array.
+
+
+// {SOLUTION}
+function solution(nums){
+  return (nums === null) ? [] : nums.sort((a,b) => a-b)
+  
+  }
+
+
+
+  ///
+  ///
