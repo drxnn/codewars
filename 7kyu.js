f1d79af6e65473658803d5d141587e35216a3dda
Drin Bratotini <drin.bratotini@gmail.com>
2023-08-04 19:32:11 -0400

    kata

--- 7kyu.js
+++ 7kyu.js
@@ -254,3 +254,31 @@ function solution(nums){
 
   ///
   ///
+///
+// Exercise 1.0
+//////
+// Simple, given a string of words, return the length of the shortest word(s).
+
+// String will never be empty and you do not need to account for different data types.
+//
+//
+// {SOLUTION}
+///////
+////
+function findShort(string) {
+  let array = string.split(" ");
+  let shortestWord = array[0];
+  for (let i = 1; i < array.length; i++) {
+    if (array[i].length < shortestWord.length) {
+      shortestWord = array[i];
+    }
+  }
+  return shortestWord.length;
+}
+
+
+
+
+//
+///////
+//
