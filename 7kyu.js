b4cc6468d4e56ad856b8ddbdcfe91c7f4231d732
Drin Bratotini <drin.bratotini@gmail.com>
2023-08-06 14:45:19 -0400

    7kyu kata 08/06/2023

--- 7kyu.js
+++ 7kyu.js
@@ -298,3 +298,41 @@ function friend(friends){
 //
 ///
 ///
+
+
+// Exercise 3.0
+
+
+// Your team is writing a fancy new text editor and you've been tasked with implementing the line numbering.
+
+// Write a function which takes a list of strings and returns each line prepended by the correct number.
+
+// The numbering starts at 1. The format is n: string. Notice the colon and space in between.
+
+// Examples: (Input --> Output)
+
+// [] --> []
+// ["a", "b", "c"] --> ["1: a", "2: b", "3: c"]
+//
+
+// {SOLUTION} //
+////////
+var number=function(array){
+  let newArr = [];
+  for(let i = 0; i < array.length; i++){
+    let element = array[i];
+    let newElement = `${i+1}: ${array[i]}`;
+    newArr.push(newElement)
+  }
+return newArr;
+}
+
+
+
+
+
+//
+///////
+
+//Exercise 4.0
+////////
