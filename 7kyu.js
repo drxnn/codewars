25a1efccbdb5dc04632e719731f7949d32299df8
Drin Bratotini <drin.bratotini@gmail.com>
2023-08-08 11:34:37 -0400

    string kata

--- 7kyu.js
+++ 7kyu.js
@@ -336,3 +336,16 @@ return newArr;
 
 //Exercise 4.0
 ////////
+////
+// Complete the solution so that it returns true if the first argument(string) passed in ends with the 2nd argument (also a string).
+//
+//{SOLUTION}////
+////
+function solution(str, ending){
+  let length = ending.length
+  return str.includes(ending, str.length - length)
+}
+  
+  
+//////
+////////
