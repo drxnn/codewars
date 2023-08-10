29bfd0f4ebf501fee3a726de59f599b11f627830
Drin Bratotini <drin.bratotini@gmail.com>
2023-08-10 11:07:48 -0400

    kata completed

--- 8kyu.js
+++ 8kyu.js
@@ -54,3 +54,21 @@ function positiveSum(arr) {
 function sumStr(a, b) {
   return (+a + +b).toString();
 }
+
+//
+//
+//Exercise 4//
+//////
+// Complete the square sum function so that it squares each number passed into it and then sums the results together.
+
+// For example, for [1, 2, 2] it should return 9
+//
+//{SOLUTION}
+function squareSum(numbers) {
+  return numbers
+    .map((x) => x ** 2)
+    .reduce((acc, curr) => {
+      acc += curr;
+      return acc;
+    }, 0);
+}
