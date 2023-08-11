80608fd86abdf60983207fcf2a9e5cccd83067a0
Drin Bratotini <drin.bratotini@gmail.com>
2023-08-11 15:28:30 -0400

    7kyu kata

--- 7kyu.js
+++ 7kyu.js
@@ -426,4 +426,29 @@ function arithmetic(a, b, operator){
      } 
       }
  
- 
+
+
+      //
+      ////
+      // Exercise:
+//       Create a function that returns the sum of the two lowest positive numbers given an array of minimum 4 positive integers. No floats or non-positive integers will be passed.
+
+// For example, when an array is passed like [19, 5, 42, 2, 77], the output should be 7.
+ //
+
+ //
+ // {SOLUTION}:
+ function sumTwoSmallestNumbers(numbers) {  
+  let firstLow = Math.min(...numbers);
+  let indexOne = numbers.indexOf(firstLow);
+  numbers.splice(indexOne, 1);
+  let secondLow = Math.min(...numbers);
+  return firstLow + secondLow;
+  
+}
+
+// second Solution:
+function sumTwoSmallestNumbers(numbers){  
+  numbers = numbers.sort(function(a, b){return a - b; });
+  return numbers[0] + numbers[1];
+};
