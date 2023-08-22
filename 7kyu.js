42a6ef50b7b911b45cc2b9853954ada6f6e7e8a2
Drin Bratotini <drin.bratotini@gmail.com>
2023-08-22 19:36:36 -0400

    exam points kata

--- 7kyu.js
+++ 7kyu.js
@@ -527,3 +527,27 @@ function roundToNext5(n){
   while(n % 5 !== 0) n++;
   return n;
 }
+
+
+
+//Exercise:
+// The first input array is the key to the correct answers to an exam, like ["a", "a", "b", "d"]. The second one contains a student's submitted answers.
+
+// The two arrays are not empty and are the same length. Return the score for this array of answers, giving +4 for each correct answer, -1 for each incorrect answer, and +0 for each blank answer, represented as an empty string (in C the space character is used).
+// If the score < 0, return 0.
+// checkExam(["a", "a", "b", "b"], ["a", "c", "b", "d"]) → 6
+// checkExam(["a", "a", "c", "b"], ["a", "a", "b",  ""]) → 7
+
+// {SOLUTION}
+function checkExam(array1, array2) {
+ let score = 0;
+ for(let i = 0; i < array1.length;i++){
+   if(array1[i] === array2[i]){
+     score += 4
+   } else if(array2[i] == ""){
+     score += 0
+   } else{
+     score -= 1
+   }
+ } return Math.max(score, 0);
+}
