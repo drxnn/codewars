987fc2ddcb7aabc668bd71ce8e2ad92d8b30eabb
Drin Bratotini <drin.bratotini@gmail.com>
2023-08-24 11:48:16 -0400

    kata

--- 8kyu.js
+++ 8kyu.js
@@ -106,3 +106,18 @@ function countSheeps(arrayOfSheeps) {
 
 // END OF EXERCISE
 //
+
+//EXERCISE:
+//Build a function that returns an array of integers from n to 1 where n>0.
+
+// Example : n=5 --> [5,4,3,2,1]
+//{SOLUTION}
+let reverseSeq = function (n) {
+  let arr = [];
+  for (let i = n; i > 0; i--) {
+    arr.push(i);
+  }
+  return arr;
+};
+
+//
