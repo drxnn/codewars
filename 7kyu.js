95c3b787bd2600d15fc9496e7b6d88242e0200d4
Drin Bratotini <drin.bratotini@gmail.com>
2023-08-25 18:40:06 -0400

    kata

--- 7kyu.js
+++ 7kyu.js
@@ -575,4 +575,24 @@ let value = s.split("").reduce((acc, curr) => {
   }
 }
 
-//
+//Exercise:
+// Several people are standing in a row divided into two teams.
+// The first person goes into team 1, the second goes into team 2, the third goes into team 1, and so on.
+
+// Task
+// Given an array of positive integers (the weights of the people), return a new array/tuple of two integers, where the first one is the total weight of team 1, and the second one is the total weight of team 2.
+//{SOLUTION}
+function rowWeights(array){
+  let result = []
+ let teamOne = array.reduce((acc, curr, i) => {
+   if(i % 2 === 0){
+     acc += curr
+   } return acc
+ }, 0)
+ let teamTwo = array.reduce((acc,curr,i)=> {
+   if(i % 2 === 1){
+     acc += curr
+   } return acc
+ }, 0)
+return [teamOne, teamTwo]
+}
