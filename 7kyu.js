256c8fef04b716f1e3de1a6090fd6436c87411fa
Drin Bratotini <drin.bratotini@gmail.com>
2023-08-27 12:39:43 -0400

    data comparison kata

--- 7kyu.js
+++ 7kyu.js
@@ -597,3 +597,26 @@ function rowWeights(array){
 return [teamOne, teamTwo]
 }
 //
+
+// Exercise:
+
+// Your task:
+// Write a function called checkCoupon which verifies that a coupon code is valid and not expired.
+
+// A coupon is no more valid on the day AFTER the expiration date. All dates will be passed as strings in this format: "MONTH DATE, YEAR".
+
+// Examples:
+// checkCoupon("123", "123", "July 9, 2015", "July 9, 2015")  ===  true
+// checkCoupon("123", "123", "July 9, 2015", "July 2, 2015")  ===  false
+//
+// {SOLUTION}
+function checkCoupon(enteredCode, correctCode, currentDate, expirationDate){
+  let dateCurr = new Date(currentDate).getTime();
+  let dateExp = new Date(expirationDate).getTime();
+  return (enteredCode === correctCode && dateCurr <= dateExp) ? true : false
+  }
+// 
+//Simpler version:
+function checkCoupon(enteredCode, correctCode, currentDate, expirationDate){
+  return enteredCode===correctCode && new Date(currentDate) <= new Date(expirationDate);
+}
