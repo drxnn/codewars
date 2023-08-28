c655d99261dd36d84e160917e92478f299125878
Drin Bratotini <drin.bratotini@gmail.com>
2023-08-28 13:09:35 -0400

    kata

--- 8kyu.js
+++ 8kyu.js
@@ -121,3 +121,21 @@ let reverseSeq = function (n) {
 };
 
 //
+
+// Exercise:
+// You will be given an array a and a value x. All you need to do is check whether the provided array contains the value.
+
+// Array can contain numbers or strings. X can be either.
+
+// Return true if the array contains the value, false if not.
+// {SOLUTION}
+//
+function check(a, x) {
+  return a.includes(x);
+}
+
+// or
+function check(a, x) {
+  return a.indexOf(x) > -1;
+}
+//
