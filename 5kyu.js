8873daeccc584f4a249469992f44c451e494a1b3
Drin Bratotini <drin.bratotini@gmail.com>
2023-08-29 19:34:50 -0400

    alphanumeric kata

--- 5kyu.js
+++ 5kyu.js
@@ -34,3 +34,28 @@ function generateHashtag(str) {
     return hashtag;
   }
 }
+
+//
+// In this example you have to validate if a user input string is alphanumeric. The given string is not nil/null/NULL/None, so you don't have to check that.
+
+// The string has the following conditions to be alphanumeric:
+
+// At least one character ("" is not valid)
+// Allowed characters are uppercase / lowercase latin letters and digits from 0 to 9
+// No whitespaces / underscore
+
+//
+//{SOLUTION}
+//
+let alphanumeric = (s) => /^[a-z\d]+$/i.test(s);
+
+// or
+function alphanumeric(string) {
+  if (!string.length) return false;
+  const allowedChar =
+    "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+  for (let el of string) {
+    if (!allowedChar.includes(el)) return false;
+  }
+  return true;
+}
