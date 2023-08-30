67bbea3ca5cccbb29f30f8baf86edfe6ecd068c4
Drin Bratotini <drin.bratotini@gmail.com>
2023-08-30 17:04:48 -0400

    sequence sum kata

--- 7kyu.js
+++ 7kyu.js
@@ -675,3 +675,26 @@ function nato(word) {
   })
  return result.trim()
 }
+
+
+//Exercise:
+//Your task is to write a function which returns the sum of a sequence of integers.
+// The sequence is defined by 3 non-negative values: begin, end, step.
+
+// If begin value is greater than the end, your function should return 0. If end is not the result of an integer number of steps, then don't add it to the sum. See the 4th example below.
+// Examples
+
+// 2,2,2 --> 2
+// 2,6,2 --> 12 (2 + 4 + 6)
+//
+//{SOLUTION}:
+const sequenceSum = (begin, end, step) => {
+  if(begin>end){
+    return 0
+  };
+  let sum = 0; 
+  for(let i = begin; i<=end; i = i+step){
+    sum += i
+  }
+  return sum
+};
