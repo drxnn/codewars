45dedd4b988941576f3589202826e7052526db32
Drin Bratotini <drin.bratotini@gmail.com>
2023-09-06 19:36:02 -0400

    simple kata

--- 8kyu.js
+++ 8kyu.js
@@ -156,3 +156,10 @@ function removeEveryOther(arr) {
 }
 // or in 1 line
 let removeEveryOther = (a) => a.filter((x, i) => i % 2 === 0);
+
+// Make a function that returns the value multiplied by 50 and increased by 6. If the value entered is a string it should return "Error".
+//{SOLUTION}
+function problem(x) {
+  if (typeof x === "string") return "Error";
+  return x * 50 + 6;
+}
