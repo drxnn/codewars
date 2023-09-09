eba81a0ccfe03c88379755aba90961555c4b7a10
Drin Bratotini <drin.bratotini@gmail.com>
2023-09-09 13:12:02 -0400

    age kata

--- 7kyu.js
+++ 7kyu.js
@@ -799,3 +799,23 @@ let getEvenNumbers = (a, callback) => {
 //
 // or
 let getEvenNums = a => a.filter(x => x%2 === 0)
+//
+
+
+// My grandfather always predicted how old people would get, and right before he passed away he revealed his secret!
+
+// In honor of my grandfather's memory we will write a function using his formula!
+
+// Take a list of ages when each of your great-grandparent died.
+// Multiply each number by itself.
+// Add them all together.
+// Take the square root of the result.
+// Divide by two.
+// Example
+// predictAge(65, 60, 75, 55, 60, 63, 64, 45) === 86
+// Note: integer should be rounded down
+
+// {SOLUTION}
+// Re-factored into 1 line
+ let  predictAge = (...ages) => Math.floor(Math.sqrt(ages.map((x) => x*x).reduce((acc, c) => acc += c)) / 2)
+  
