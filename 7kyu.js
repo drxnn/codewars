2ba61a9858ba1398070d7379bc664225e5a47d5a
Drin Bratotini <drin.bratotini@gmail.com>
2023-10-01 13:19:13 -0400

    kata

--- 7kyu.js
+++ 7kyu.js
@@ -913,3 +913,13 @@ const bump=x=>x.split('n').length>16?"Car Dead":"Woohoo!"
 
 //
 //
+
+
+
+//
+//
+// Write a small function that returns the values of an array that are not odd.
+// All values in the array will be integers. Return the good values in the order they are given.
+function noOdds( values ){
+  return values.filter((x) => x % 2 ===0)
+}
