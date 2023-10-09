e392c79d7ab761a3c13db8883dcf5546717ad109
Drin Bratotini <drin.bratotini@gmail.com>
2023-10-09 18:42:14 -0400

    7kyu kata

--- 7kyu.js
+++ 7kyu.js
@@ -997,3 +997,21 @@ function vowelIndices(word){
 }
 
 //
+
+
+// Given an array of numbers, return a new array of length number containing the last even numbers from the original array (in the same order). The original array will be not empty and will contain at least "number" even numbers.
+
+// For example:
+
+// ([1, 2, 3, 4, 5, 6, 7, 8, 9], 3) => [4, 6, 8]
+// ([-22, 5, 3, 11, 26, -6, -7, -8, -9, -8, 26], 2) => [-8, 26]
+
+//{SOLUTION}
+function evenNumbers(array, number) {
+  let even = array.filter(n => n % 2 === 0);
+  return even.slice(-number)
+  
+}
+
+// in one line:
+let evenNumbers =(a, n) => a.filter(n => n % 2 === 0).slice(-n)
