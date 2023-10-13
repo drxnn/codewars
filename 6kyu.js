0ff0388a10e16f2e49d2a6135e33fb8f8c6ec834
Drin Bratotini <drin.bratotini@gmail.com>
2023-10-13 14:32:21 -0400

    find nearest palindrome num

--- 6kyu.js
+++ 6kyu.js
@@ -233,3 +233,48 @@ function sortArray(array) {
 }
 
 //
+//Exercise:
+//A palindrome is a word, phrase, number, or other sequence of characters which reads the same backward as forward. Examples of numerical palindromes are:
+
+// 2332
+// 110011
+// 54322345
+
+// For a given number num, return its closest numerical palindrome which can either be smaller or larger than num. If there are 2 possible values, the larger value should be returned. If num is a numerical palindrome itself, return it.
+
+// For this kata, single digit numbers will NOT be considered numerical palindromes.
+
+// Also, you know the drill - be sure to return "Not valid" if the input is not an integer or is less than 0.
+
+// palindrome(8) => 11
+// palindrome(281) => 282
+// palindrome(1029) => 1001
+// palindrome(1221) => 1221
+// palindrome("1221") => "Not valid"
+//
+//{SOLUTION}
+function palindrome(num) {
+  if (typeof num !== "number" || num < 0) return "Not valid";
+  let checkPalindrome = (num) => {
+    let numString = num.toString().split("");
+
+    let reverse = [...numString].reverse();
+
+    return numString.join("") == reverse.join("");
+  };
+  let added = 1;
+  let findClosest = (num, added) => {
+    if (checkPalindrome(num + added) === true) {
+      return num + added;
+    } else if (checkPalindrome(num - added) === true) {
+      return num - added;
+    } else {
+      added += 1;
+      return findClosest(num, added);
+    }
+  };
+  if (num < 10) num = 10;
+  if (checkPalindrome(num)) return num;
+
+  return findClosest(num, (added = 1));
+}
