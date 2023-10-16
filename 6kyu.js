a1d5fb6f1e19e1912d6a13b8cf5298c24f6012c8
Drin Bratotini <drin.bratotini@gmail.com>
2023-10-16 16:32:25 -0400

    create NxN array

--- 6kyu.js
+++ 6kyu.js
@@ -278,3 +278,35 @@ function palindrome(num) {
 
   return findClosest(num, (added = 1));
 }
+
+//
+// Your task, is to create N×N multiplication table, of size provided in parameter.
+
+// For example, when given size is 3:
+
+// 1 2 3
+// 2 4 6
+// 3 6 9
+// For the given example, the return value should be:
+
+// [[1,2,3],[2,4,6],[3,6,9]]
+
+//{SOLUTION}
+//
+let multiplicationTable = (size) => {
+  let container = [];
+  let myArr = [];
+  for (let i = 1; i <= size; i++) {
+    myArr.push(i);
+  }
+
+  for (let i = 1; i <= size; i++) {
+    container.push([...myArr]);
+  }
+  return container.map((innerArray, i) => {
+    return innerArray.map((x) => {
+      x = x * (i + 1);
+      return x;
+    });
+  });
+};
