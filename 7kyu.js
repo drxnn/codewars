8b39c1bfd59e14c5c9b25b98eb2df6831a3b0659
Drin Bratotini <drin.bratotini@gmail.com>
2023-10-24 14:59:29 -0400

    series sum

--- 7kyu.js
+++ 7kyu.js
@@ -1015,3 +1015,41 @@ function evenNumbers(array, number) {
 
 // in one line:
 let evenNumbers =(a, n) => a.filter(n => n % 2 === 0).slice(-n)
+
+
+
+//
+// TASK
+// Your task is to write a function which returns the sum of following series upto nth term(parameter).
+
+// Series: 1 + 1/4 + 1/7 + 1/10 + 1/13 + 1/16 +...
+// Rules:
+// You need to round the answer to 2 decimal places and return it as String.
+
+// If the given value is 0 then it should return 0.00
+
+// You will only be given Natural Numbers as arguments.
+
+// Examples:(Input --> Output)
+// 1 --> 1 --> "1.00"
+// 2 --> 1 + 1/4 --> "1.25"
+// 5 --> 1 + 1/4 + 1/7 + 1/10 + 1/13 --> "1.57"
+//
+//SOLUTION:
+function SeriesSum(n)
+{
+  let result = 0
+   let num = 1;
+  for(let i = 0; i<n; i++){
+   
+    console.log(num)
+    result +=  (1/num);
+    num += 3
+    console.log(num)
+  }
+  return result.toFixed(2)
+}
+//
+//Recursive solution:
+
+
