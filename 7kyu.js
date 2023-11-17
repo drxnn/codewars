eadbecddf78a75b66f43fa09e46974ce73a54719
Drin Bratotini <drin.bratotini@gmail.com>
2023-11-16 20:08:28 -0500

    number length kata

--- 7kyu.js
+++ 7kyu.js
@@ -1169,3 +1169,14 @@ var SequenceSum = (function() {
   return SequenceSum;
 
 })();
+
+
+// TASK:
+// Determine the total number of digits in the integer (n>=0) given as input to the function. For example, 9 is a single digit, 66 has 2 digits and 128685 has 6 digits. Be careful to avoid overflows/underflows.
+
+// All inputs will be valid.
+
+// {SOLUTION}
+function digits(n) {
+  return n.toString().split("").length
+}
