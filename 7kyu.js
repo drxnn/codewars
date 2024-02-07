dde20b8869505c85513df5ed2abfc1e6a4f7778f
Drin Bratotini <drin.bratotini@gmail.com>
2024-02-07 14:39:44 -0500

    back

--- 7kyu.js
+++ 7kyu.js
@@ -1180,3 +1180,28 @@ var SequenceSum = (function() {
 function digits(n) {
   return n.toString().split("").length
 }
+
+
+// Re-practice problem:
+// In mathematics, the factorial of a non-negative integer n, denoted by n!, is the product of all positive integers less than or equal to n. For example: 5! = 5 * 4 * 3 * 2 * 1 = 120. By convention the value of 0! is 1.
+
+// Write a function to calculate factorial for a given input. 
+//{SOLUTION}
+let factorial = n => {
+  let arr = []
+for(let i = 1; i<=n;i++){
+  arr.push(i);
+}
+if(n==0 || n==1){
+return 1
+} else if(n<0 || n>12){
+throw new RangeError("Cannot factorize")
+} else{
+    return arr.reduce((acc,c) => {
+  acc *= c;
+  return acc
+})
+} 
+
+
+}
