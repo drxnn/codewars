8e0b8602126eabffa4009d2421f62861ce9ccbe2
Drin Bratotini <drin.bratotini@gmail.com>
2025-03-08 13:57:31 -0500

    reverse linked list problem

---
+++ leetcode/reverseLinkedList.ts
@@ -0,0 +1,23 @@
+// Given the head of a singly linked list, reverse the list, and return the reversed list.
+
+class ListNode {
+  val: number;
+  next: ListNode | null;
+  constructor(val?: number, next?: ListNode | null) {
+    this.val = val === undefined ? 0 : val;
+    this.next = next === undefined ? null : next;
+  }
+}
+
+// Solution:
+function reverseList(head: ListNode | null): ListNode | null {
+  let prev: ListNode | null = null;
+  let curr = head;
+  while (curr) {
+    let next = curr.next;
+    curr.next = prev;
+    prev = curr;
+    curr = next;
+  }
+  return prev;
+}
